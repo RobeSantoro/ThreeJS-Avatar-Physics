e4c771ec252a1b5c5e1a3073e3588d952a1f282d
RobeSantoro <santoro.robe@gmail.com>
2022-03-16 22:19:09 +0100

    OOP Rules

--- cube.js
+++ cube.js
@@ -1,21 +1,36 @@
 import * as THREE from 'three';
+import * as CANNON from 'cannon-es'
 
 export class Cube {
   constructor(params) {
-    this._CreateCube(params)
+    this._CreateThreeCube(params)
+    this._CreateCannonCube(params)
   }
     
-  _CreateCube(params) {
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
+  _CreateThreeCube(params) {
+    const geometry = new THREE.BoxGeometry(params.cubeSize.x, params.cubeSize.y, params.cubeSize.z)
     const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 })
-    const cube = new THREE.Mesh(geometry, material)
-    params.add(cube)
+    this.cube = new THREE.Mesh(geometry, material)
+    this.cube.position.y = (params.cubePosition.y * 0.5)
+    params.scene.add(this.cube)
   }
 
   _CreateCannonCube(params) {
-    const shape = new CANNON.Box(new CANNON.Vec3(1, 1, 1))
-    const body = new CANNON.Body({ mass: 0 })
-    body.addShape(shape)
+    const cubeShape = new CANNON.Box(new CANNON.Vec3(params.cubeSize.x * .5, params.cubeSize.y * .5, params.cubeSize.z * .5))
+    this.cubeBody = new CANNON.Body({
+      mass: 1,
+      position: params.cubePosition,
+      shape: cubeShape,
+      //quaternion: new CANNON.Quaternion().setFromEuler(45, 0, 0),
+      material: params.defaultMaterial
+    })
+    params.world.addBody(this.cubeBody)
+  }
+
+  _Update() {
+    this.cube.position.copy(this.cubeBody.position)
+    this.cube.quaternion.copy(this.cubeBody.quaternion)
   }
 
 }
+
--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-<<<<<<< HEAD
 
-//import { Cube } from './cube.js'
-=======
-import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js'
 import { Cube } from './cube.js'
->>>>>>> 55221f28ab3234e13989df22c3544025af6aa19c
 import * as CANNON from 'cannon-es'
 
 import Stats from 'three/examples/jsm/libs/stats.module.js'
@@ -20,33 +15,13 @@ const scene = new THREE.Scene()
 
 // Add grid helper and axis helper
 const gridHelper = new THREE.GridHelper(10, 10)
-const axesHelper = new THREE.AxesHelper(5)
-scene.add(gridHelper, axesHelper)
+scene.add(gridHelper)
 
 // Add a light
 const light = new THREE.DirectionalLight(0xffffff, 1)
 light.position.set(-1, 3, 4)
 
 scene.add(light)
-
-/////////////////////////////////////////////////////////// Cube Params
-
-let cubePosition = new THREE.Vector3(0, 1, 0)
-let cubeSize = new THREE.Vector3(1, 1, 1)
-
-//////////////////////////////////////////////////////// Three.js World
-
-// Create a cube
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(cubeSize.x, cubeSize.y, cubeSize.z),
-  new THREE.MeshStandardMaterial({
-    color: 0x00ff00,
-    roughness: 1,
-    metalness: 0
-  })
-)
-cube.position.y = (cubePosition.y * 0.5)
-scene.add(cube)
 
 ////////////////////////////////////////////////// Cannon Physics World
 
@@ -63,11 +38,7 @@ const defaultcontactMaterial = new CANNON.ContactMaterial(
   defaultMaterial,
   defaultMaterial,
   {
-<<<<<<< HEAD
     friction: 0.1,
-=======
-    friction: 0,
->>>>>>> 55221f28ab3234e13989df22c3544025af6aa19c
     restitution: 0
   }
 )
@@ -83,30 +54,41 @@ const floorBody = new CANNON.Body({
 })
 world.addBody(floorBody)
 
-///////////////////////////////////////////////////// Cube cannon shape
+/////////////////////////////////////////////////////////// Cube Params
 
-const cubeShape = new CANNON.Box(new CANNON.Vec3(cubeSize.x * .5, cubeSize.y * .5, cubeSize.z * .5))
-// Cube cannon body
-const cubeBody = new CANNON.Body({
-  mass: 1,
-  position: cubePosition,
-  shape: cubeShape,
-  //quaternion: new CANNON.Quaternion().setFromEuler(45, 0, 0),
+const params = {
+  scene: scene,
+  world: world,
+  cubePosition: new THREE.Vector3(-1, 1, 0),
+  cubeSize:new THREE.Vector3(.5, .5, .5),
+  material: defaultMaterial
+}
+
+const cube = new Cube(params)
+
+const cube2 = new Cube({
+  scene: scene,
+  world: world,
+  cubePosition: new THREE.Vector3(1, 1, 0),
+  cubeSize:new THREE.Vector3(.5, .5, .5),
   material: defaultMaterial
 })
-world.addBody(cubeBody)
+
+const cube3 = new Cube({
+  scene: scene,
+  world: world,
+  cubePosition: new THREE.Vector3(0, 1, 0),
+  cubeSize:new THREE.Vector3(.5, .5, .5),
+  material: defaultMaterial
+})
+
 
 //////////////////////////////////////////////////////////////// Camera
 
 // Camera
 const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.set(0, 5, 10)
-camera.lookAt(new THREE.Vector3(cubePosition.x, cubePosition.y, cubePosition.z))
-
-const cameraGroup = new THREE.Group()
-cameraGroup.position.set(0, 0, 0)
-cameraGroup.add(camera)
-//scene.add(cameraGroup) // Added by controls.getObject()
+camera.lookAt(new THREE.Vector3(params.cubePosition.x, params.cubePosition.y, params.cubePosition.z))
 
 ////////////////////////////////////////////////////////////// Renderer
 
@@ -123,11 +105,8 @@ renderer.render(scene, camera)
 const canvas = renderer.domElement
 document.body.appendChild(canvas)
 
-////////////////////////////////////////////////// Pointerlock Controls
-
-const controls = new PointerLockControls(cameraGroup, canvas)
-//console.log(controls.getObject())
-scene.add(controls.getObject());
+// Orbit Controls
+new OrbitControls(camera, renderer.domElement)
 
 ///////////////////////////////////////////////////// Keyboard Controls
 
@@ -138,7 +117,6 @@ let keyControls = {
   right: false,
   jump: false
 }
-
 
 // Add event listener to keydown
 document.addEventListener('keydown', (event) => {
@@ -162,16 +140,8 @@ document.addEventListener('keydown', (event) => {
       break
   }
 
-};
-
-document.addEventListener('keydown', onKeyDown);
-document.addEventListener('keyup', onKeyUp);
-
-canvas.addEventListener('click', function () {
-  controls.lock()
 })
 
-<<<<<<< HEAD
 // Add event listener to keyup
 document.addEventListener('keyup', (event) => {
   switch (event.key) { // Keycode deprecated ?????
@@ -197,106 +167,41 @@ document.addEventListener('keyup', (event) => {
 })
 
 const vel = 0.2
-=======
-////////////////////////////////////////////////////////// Animate Loop
->>>>>>> 55221f28ab3234e13989df22c3544025af6aa19c
 
 
 const clock = new THREE.Clock()
 let oldElapsedTime = 0
 
 let prevTime = performance.now();
-//console.log(prevTime);
 
 function animate() {
 
-<<<<<<< HEAD
   // Update the stats
   stats.begin()
 
-=======
-  // Call the animate function again on the next frame
-  requestAnimationFrame(animate)
-
-  /* // CANNON Physics
->>>>>>> 55221f28ab3234e13989df22c3544025af6aa19c
   const elapsedTime = clock.getElapsedTime()
   const deltaTime = elapsedTime - oldElapsedTime
   oldElapsedTime = elapsedTime
 
   // Update Cannon World
-<<<<<<< HEAD
   world.step(1/60, deltaTime, 3)
-  cube.position.copy(cubeBody.position)
-  cube.quaternion.copy(cubeBody.quaternion)
-=======
-  world.step(1 / 60, deltaTime, 3) */
 
-  // Update cube position
-  // cube.position.copy(cubeBody.position)
-
-  // Update camera position
-  const time = performance.now();
-
-  if (controls.isLocked === true) {
-
-    const delta = (time - prevTime) / 1000;
-
-    velocity.x -= velocity.x * 0.001 * delta;
-    velocity.z -= velocity.z * 0.001 * delta;
-
-    
-    //velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
-    
-    direction.z = Number(moveForward) - Number(moveBackward);
-    direction.x = Number(moveRight) - Number(moveLeft);
-    direction.normalize(); // this ensures consistent movements in all directions
-    
-    if (moveForward || moveBackward) velocity.z -= direction.z * 1 * delta;
-    if (moveLeft || moveRight) velocity.x -= direction.x * 1 * delta;
-    
-    //console.log(velocity.x, velocity.z);
-    /* if (onObject === true) {
-
-      velocity.y = Math.max(0, velocity.y);
-      canJump = true;
-
-    } */
-
-    controls.moveRight(- velocity.x * delta);
-    controls.moveForward(- velocity.z * delta);
-
-    //controls.getObject().position.y += (velocity.y * delta); // new behavior
-
-    /*     if (controls.getObject().position.y < 10) {
-    
-          velocity.y = 0;
-          controls.getObject().position.y = 10;
-    
-          canJump = true;
-    
-        } */
-
-
-  }
-
-  prevTime = time;
->>>>>>> 55221f28ab3234e13989df22c3544025af6aa19c
+  cube._Update(deltaTime)
+  cube2._Update(deltaTime)
+  cube3._Update(deltaTime)
 
   // Update Key Controls
   if (keyControls.foward) {    
-    cubeBody.velocity.z -= vel
+    cube.cubeBody.velocity.z -= vel
   } else if (keyControls.backward) {
-    cubeBody.velocity.z += vel
+    cube.cubeBody.velocity.z += vel
   }
 
   if (keyControls.left) {
-    cubeBody.velocity.x -= vel
+    cube.cubeBody.velocity.x -= vel
   } else if (keyControls.right) {
-    cubeBody.velocity.x += vel
+    cube.cubeBody.velocity.x += vel
   }
-
-  console.log(keyControls);
 
   // Render the Scene
   renderer.render(scene, camera)
@@ -304,14 +209,10 @@ function animate() {
   // Update the controls  
   //console.log(controls.getObject().position.x, controls.getObject().position.y, controls.getObject().position.z);
 
-
-<<<<<<< HEAD
   // Call the animate function again on the next frame
   requestAnimationFrame(animate)
 
   stats.end()
-=======
->>>>>>> 55221f28ab3234e13989df22c3544025af6aa19c
 }
 
 animate()
