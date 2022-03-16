aa814ccc7ea4fb903840169e6dd62765be92980f
RobeSantoro <santoro.robe@gmail.com>
2022-03-16 17:53:40 +0100

    Rolling Cube works

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
 //import { Cube } from './cube.js'
 import * as CANNON from 'cannon-es'
+
+import Stats from 'three/examples/jsm/libs/stats.module.js'
+
+// Add Stats
+const stats = new Stats()
+document.body.appendChild(stats.dom)
 
 // Create the Scene
 const scene = new THREE.Scene()
@@ -67,7 +74,7 @@ const defaultcontactMaterial = new CANNON.ContactMaterial(
   defaultMaterial,
   defaultMaterial,
   {
-    friction: 0.001,
+    friction: 0.1,
     restitution: 0
   }
 )
@@ -102,35 +109,69 @@ world.addBody(cubeBody)
 const clock = new THREE.Clock()
 let oldElapsedTime = 0
 
-// Add event listener to keypresses
+let keyControls = {
+  foward: false,
+  backward: false,
+  left: false,
+  right: false,
+  jump: false
+}
+
+
+// Add event listener to keydown
 document.addEventListener('keydown', (event) => {
   switch (event.key) { // Keycode deprecated ?????
     case 'w':
-      cubeBody.velocity.set(0, 2, 0)
+      keyControls.foward = true
       break
     case 'a':
-      cubeBody.velocity.set(-2, 0, 0)
+      keyControls.left = true
       break
     case 's':
-      cubeBody.velocity.set(0, -2, 0)
+      keyControls.backward = true
       break
     case 'd':
-      cubeBody.velocity.set(2, 0, 0)
+      keyControls.right = true
       break
-    case 'q':
-      cubeBody.velocity.set(0, 0, 2)
-      break 
-    case 'e':
-      cubeBody.velocity.set(0, 0, -2)
+    case 'space':
+      keyControls.jump = true
       break
     default:
       break
   }
 })
 
+// Add event listener to keyup
+document.addEventListener('keyup', (event) => {
+  switch (event.key) { // Keycode deprecated ?????
+    case 'w':
+      keyControls.foward = false
+      break
+    case 'a':
+      keyControls.left = false
+      break
+    case 's':
+      keyControls.backward = false
+      break
+    case 'd':
+      keyControls.right = false
+      break
+    case 'space':
+      keyControls.jump = false
+      break
+    default:
+      break
+  }
+
+})
+
+const vel = 0.2
 
 
 function animate() {
+
+  // Update the stats
+  stats.begin()
 
   const elapsedTime = clock.getElapsedTime()
   const deltaTime = elapsedTime - oldElapsedTime
@@ -138,12 +179,23 @@ function animate() {
 
   // Update Cannon World
   world.step(1/60, deltaTime, 3)
-
-  // Update cube position
-  //cube.position.y = cubeBody.position.y
-  //console.log(cubeBody.position.y);
   cube.position.copy(cubeBody.position)
   cube.quaternion.copy(cubeBody.quaternion)
+
+  // Update Key Controls
+  if (keyControls.foward) {    
+    cubeBody.velocity.z -= vel
+  } else if (keyControls.backward) {
+    cubeBody.velocity.z += vel
+  }
+
+  if (keyControls.left) {
+    cubeBody.velocity.x -= vel
+  } else if (keyControls.right) {
+    cubeBody.velocity.x += vel
+  }
+
+  console.log(keyControls);
 
   // Render the Scene
   renderer.render(scene, camera)
@@ -153,7 +205,10 @@ function animate() {
 
   // Call the animate function again on the next frame
   requestAnimationFrame(animate)
+
+  stats.end()
 }
+
 animate()
 
 // Add a listener to the window resize event
